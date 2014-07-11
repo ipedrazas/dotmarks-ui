ab152160c6f7d61a13f5ed5603293aeb1934f28d
Ivan Pedrazas <ipedrazas@gmail.com>
2014-07-11 22:29:25 +0100

    cleanup

--- dist/app/js/app.js
+++ dist/app/js/app.js
@@ -2,15 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('dotApp', ['ngRoute','ui.bootstrap', 'LocalStorageModule', 'angularFileUpload'])
+angular.module('dotApp', ['ngRoute','ui.bootstrap', 'angularFileUpload'])
 .config(['$routeProvider', '$httpProvider' ,function ($routeProvider, $httpProvider) {
 	  $routeProvider
           .when('/dotmarks', {templateUrl: 'partials/dotmarks.html', controller: 'dotMarkController'})
           .when('/bulk_import', {templateUrl: 'partials/bulk_import.html', controller: 'terminalCtl'})
           .when('/upload_html', {templateUrl: 'partials/upload_html.html', controller: 'terminalCtl'})
-          .when('/edit', {templateUrl: 'partials/editDotMark.html', controller: 'dotMarkController'})
-          .when('/applications', {templateUrl: 'partials/applications.html', controller: 'appsCtl'})
-          .when('/settings', {templateUrl: 'partials/settings.html', controller: 'settingsCtl'})
 		.otherwise({redirectTo: '/dotmarks'});
 
 }]);
